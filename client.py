e4642df4114074826f6364763fcf0a55fca4fa4b
root <root@raspberrypi2.mshome.net>
2022-10-29 13:04:14 +0100

    nambahin kodingan buat data dari sensor

--- client.py
+++ client.py
@@ -1,6 +1,20 @@
 import time
 import socketio
 import random
+import time
+import board
+import adafruit_dht
+import psutil
+
+sensor_temp = 0
+sensor_humidity = 0
+
+#for proc in psutil.process_iter():
+    #print(proc)
+#    if proc.name() == 'libgpiod_pulsein' or proc_name() == 'libgpiod_pulsei':
+#        proc.kill()
+
+sensor = adafruit_dht.DHT11(board.D23)
 
 thread = None
 sio = socketio.Client(logger=False, engineio_logger=False)
@@ -11,7 +25,7 @@ def background_thread():
         b = random.randrange(10)
         if b > 5:
             print(b)
-            send_coba()
+            send_sensorData()
             sio.sleep(5)
 
 
@@ -24,8 +38,26 @@ def connect():
         sio.start_background_task(background_thread)
 
 @sio.event
-def send_coba():
-    sio.emit('cobadariclient', {'data': 'BISA GAKK'})
+def send_sensorData():
+    try:
+        temp = sensor.temperature
+        humidity = sensor.humidity
+        global sensor_temp
+        global sensor_humidity
+        
+        sensor_temp = temp
+        sensor_humidity = humidity
+        print("temperature: {}C humidity: {}% ".format(temp, humidity))
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(1.0)
+        #continue
+    except Exception as error:
+        sensor.exit()
+        raise error
+    time.sleep(1.0)
+    
+    sio.emit('cobadariclient', {'data_temp': sensor_temp, 'data_humid': sensor_humidity})
 
 @sio.event
 def coba_dari_server(data):
@@ -33,6 +65,6 @@ def coba_dari_server(data):
 
 
 if __name__ == '__main__':
-    sio.connect('http://localhost:5000')
+    sio.connect('http://192.168.137.185:5000')
 
     # sio.wait()
